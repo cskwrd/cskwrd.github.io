0818004b0d74447828bb548b56c684e54fd22a11
cskwrd <cskwrd@users.noreply.github.com>
2024-10-16 01:48:10 +0000

    deploy: cskwrd/site@57db4fe7307b2c1660dc2908fc4d5009843cd315

--- dist/escpfdp1Xi.js
+++ dist/escpfdp1Xi.js
@@ -217,3 +217,27 @@ class HeadingAnchors extends HTMLElement {
 HeadingAnchors.register();
 
 export { HeadingAnchors }
+function updateDate() {
+    const now = new Date();
+    const dateOptions = { 
+        weekday: 'short', 
+        day: '2-digit', 
+        month: 'short'
+    };
+    const timeOptions = {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    };
+    
+    const dateString = now.toLocaleDateString('en-GB', dateOptions).replace(/,/g, '');
+    let timeString = now.toLocaleTimeString('en-GB', timeOptions);
+    timeString = timeString.replace(/(:)(?=[^:]*$)/, '/');
+    
+    const dateElement = document.getElementById('current-date');
+    dateElement.innerHTML = `<span>${dateString}</span>&nbsp;<span class="time">${timeString}</span>`;
+    dateElement.style.display = 'flex';  // Show the date/time element
+}
+updateDate();
+setInterval(updateDate, 1000);
